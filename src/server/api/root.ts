b4146c17489ba18fa38b66eb2b7a69cd3a963a51
thewisepup <wisepup257@gmail.com>
2025-04-15 16:56:59 -0700

    remove postRouter from t3 init

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { podcastRouter } from "./routers/podcast";
 
@@ -8,7 +7,6 @@ import { podcastRouter } from "./routers/podcast";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   podcast: podcastRouter,
 });
 
